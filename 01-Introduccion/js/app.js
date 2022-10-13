de14e2a72558d5b7c31a723776c9da8cb0515683
maalroba22 <maalroba22@gmail.com>
2022-10-12 23:09:05 -0500

    constates y variables termnados

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -2,12 +2,20 @@
  * @description: Toma el nombre y lo pinta en el h1
  */
 nombre = document.querySelector('.nombre')
+tehoria = document.querySelector('#tehoria')
 
-
-nombre.innerHTML =`
-Hola Manuel Romero 
+nombre.innerHTML = `
+    Hola Manuel Romero 
 `
- const alertica =()=>{
+const alertica = () => {
     alert('!hola tengo el gusto de informarle que estas aprendiendo js..')
- }
+}
+
+tehoria.innerHTML = `
+                    <ul class="list-disc">
+                            <li>Una constante es un elemento de datos con nombre con un valor predefinido</li>
+                            <li>una variable es un elemento de datos con nombre cuyo valor puede cambiar durante el curso de la ejecución de un programa. Las variables pueden ser globales o locales.</li>
+                    </ul>
+ 
+ `
 
