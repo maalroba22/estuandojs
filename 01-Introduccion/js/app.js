0d31dc0fdcbf9e79c30b19c6e4ab9bb2600244b7
maalroba22 <maalroba22@gmail.com>
2022-10-12 22:34:37 -0500

    ejercicio 1 llamar js en html

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,13 @@
-alert('Hola manuel Romero')
+/**
+ * @description: Toma el nombre y lo pinta en el h1
+ */
+nombre = document.querySelector('.nombre')
+
+
+nombre.innerHTML =`
+Hola Manuel Romero 
+`
+ const alertica =()=>{
+    alert('!hola tengo el gusto de informarle que estas aprendiendo js..')
+ }
+
